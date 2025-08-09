a1af933a2518b22c106809261d8cb5255c2c9441
Nazil1712 <ndhalwala@gmail.com>
2025-08-09 19:53:53 +0530

    test

--- src/features/messages/messageApi.js
+++ src/features/messages/messageApi.js
@@ -1,6 +1,8 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function fetchMessagesByUserId(userId) {
   // console.log("Called Me")
-    const response = await fetch(`/api/messages/${userId}`);
+    const response = await fetch(`${API_URL}api/messages/${userId}`);
     const data = await response.json();
     console.log("Data", data);
     return { data };
--- src/features/user/userApi.js
+++ src/features/user/userApi.js
@@ -2,7 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function fetchAllUsers() {
   // console.log("Called Me")
-    const response = await fetch(`${API_URL}/api/users/`);
+    const response = await fetch(`${API_URL}api/users/`);
     const data = await response.json();
     // console.log("Data", data);
     return { data };
@@ -11,7 +11,7 @@ export async function fetchAllUsers() {
 
 export async function fetchUserById(userId) {
   // console.log("Called Me")
-    const response = await fetch(`${API_URL}/api/users/${userId}`);
+    const response = await fetch(`${API_URL}api/users/${userId}`);
     const data = await response.json();
     // console.log("Data", data);
     return { data };
